971146a4d10d89493dd710443c4a9dfdfc3376f9
monsoonssn <22012050+monsoonssn@users.noreply.github.com>
2021-02-23 20:34:29 -0700

    operate function

--- calc.js
+++ calc.js
@@ -15,3 +15,20 @@ const multiply = (a, b) => {
 const divide = (a, b) => {
   return a / b;
 };
+
+const operate = (a, b, operator) => {
+  switch (operator) {
+    case operator == "+":
+      add(a, b);
+      break;
+    case operator == "-":
+      subtract(a, b);
+      break;
+    case operator == "*":
+      multiply(a, b);
+      break;
+    case operator == "/":
+      divide(a, b);
+      break;
+  }
+};
