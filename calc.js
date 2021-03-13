1970d335441b3104abcd47750a6bd2c7916a9081
monsoonssn <22012050+monsoonssn@users.noreply.github.com>
2021-03-12 18:22:34 -0700

    adding some jsdoc documentation

--- calc.js
+++ calc.js
@@ -3,16 +3,59 @@ let prevValue = "";
 let currentValue = display.innerText;
 let operator = "";
 
+/**
+ * Adds two numbers together
+ *
+ * @param {number} a first operand
+ * @param {number} b second operand
+ * @returns The sum of the addition
+ */
 const add = (a, b) => a + b;
 
+/**
+ * Subtracts two numbers
+ *
+ * @param {number} a first operand
+ * @param {number} b second operand
+ * @returns The difference of the subtraction
+ */
 const subtract = (a, b) => a - b;
 
+/**
+ * Multiplies two numbers together
+ *
+ * @param {number} a first operand
+ * @param {number} b second operand
+ * @returns The product of the multiplication
+ */
 const multiply = (a, b) => a * b;
 
+/**
+ * Divides two numbers together
+ *
+ * @param {number} a first operand
+ * @param {number} b second operand
+ * @returns The quotient of the division
+ */
 const divide = (a, b) => a / b;
 
+/**
+ * Gets the power of the first operand to the second operand
+ * @param {number} a first operand
+ * @param {number} b second operand
+ * @returns The power of the exponentiation
+ */
 const power = (a, b) => Math.pow(a, b);
 
+/**
+ * Takes in two operands and an operator and applies that operation and
+ * returns the result
+ *
+ * @param {string} a the first operand
+ * @param {string} b the second operand
+ * @param {string} operator the operator to be used in the operation
+ * @returns the result of the operation
+ */
 const operate = (a, b, operator) => {
   let output;
   switch (operator) {
